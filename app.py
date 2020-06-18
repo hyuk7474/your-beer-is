ad01b42704c027a993c282b8cba86a864a140924
hyuk7474 <65122201+hyuk7474@users.noreply.github.com>
2020-06-18 22:29:29 +0900

    일부 DB 구축

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017) # client가 robo와 같은 역할(mongodb에 연결)
+db = client.your_beer_is
 # from selenium import webdriver
 
 # driver = webdriver.Chrome(r"C:\Users\LG\Downloads\chromedriver_win32\chromedriver")
@@ -11,23 +15,74 @@ from bs4 import BeautifulSoup
 
 # driver.page_source
 
-target_url = 'https://dev.bjcp.org/style/2015/beer/?'
+for i in range(1, 32):
+    target_url = 'https://dev.bjcp.org/style/2015/beer/?pg=' + str(i)
 
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get(target_url, headers=headers)
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(target_url, headers=headers)
 
 
-soup = BeautifulSoup(data.text, 'html.parser')
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-beer_styles = soup.select('#content > div > div.blog.paginated.home-loop > div > article')
+    beer_styles = soup.select('#content > div > div.blog.paginated.home-loop > div > article')
 
-for beer_style in beer_styles:
-    title_tag = beer_style.select_one('header > h1 > a')
-    info_tag = beer_style.select_one('div > p')
-    title = title_tag['title']
-    
-    print(info_tag.text)
-    #content > div > div.blog.paginated.home-loop > div
+    for beer_style in beer_styles:
+        title_tag = beer_style.select_one('header > h1 > a')
+        info_tag = beer_style.select_one('div > p')
+        IBU_tag = beer_style.select_one('div > div.vital-statistics > div > div:nth-child(1) > div:nth-child(2) > p')
+        ABV_tag = beer_style.select_one('div > div.vital-statistics > div > div:nth-child(5) > div:nth-child(2) > p')
+        aroma_tag = beer_style.select_one('div > div.aroma > p:nth-child(2)')
+        flavor_tag = beer_style.select_one('div > div.flavor > p:nth-child(2)')
+        history_tag = beer_style.select_one('div > div.history > p')
 
-# print(beer_style)
+        if IBU_tag is not None:
+            print()
+        else:
+            continue
+
+        if ABV_tag is not None:
+            print()
+        else:
+            continue
+
+        if aroma_tag is not None:
+            print()
+        else:
+            continue
+
+        if flavor_tag is not None:
+            print()
+        else:
+            continue     
+
+        if history_tag is not None:
+            print()
+        else:
+            continue  
+
+        title = title_tag.text
+        info = info_tag.text
+        IBU = IBU_tag.text
+        ABV = ABV_tag.text
+        aroma = aroma_tag.text
+        flavor = flavor_tag.text
+        history = history_tag.text
+        
+        document = {
+            'title': title,
+            'IBU': IBU,
+            'ABV': ABV,
+            'aroma': aroma,
+            'flavor': flavor,
+            'info': info,
+            'history': history
+        }
+
+        db.bjcp.insert_one(document)
+
+
+        # print(title, IBU, ABV, aroma, flavor, info, history)
+
+    time.sleep(0.3)  # 0.3초 대기(차단당하지 않도록)
+    # print(beer_style)
---
+++ sample.py
@@ -0,0 +1,6 @@
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+    else:
+        continue
+    print('검사 끝!')
