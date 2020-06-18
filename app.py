c4db6a080e840d4192cdd1a27fa1151d66116a2c
hyuk7474 <65122201+hyuk7474@users.noreply.github.com>
2020-06-18 19:53:44 +0900

    크롤링 기초 마련

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+# from selenium import webdriver
 
+# driver = webdriver.Chrome(r"C:\Users\LG\Downloads\chromedriver_win32\chromedriver")
+# driver.implicitly_wait(3)
+# driver.get('https://www.brewersfriend.com/homebrew/recipe/calculator')
+
+# driver.find_element_by_class_name('text')
+# driver.find_element_by_css_selector('#calsetup > div > div.recipeEditRight > div > div > div.text')
+
+# driver.page_source
 
 target_url = 'https://dev.bjcp.org/style/2015/beer/?'
 
@@ -13,11 +22,12 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 beer_styles = soup.select('#content > div > div.blog.paginated.home-loop > div > article')
 
-
-
 for beer_style in beer_styles:
     title_tag = beer_style.select_one('header > h1 > a')
+    info_tag = beer_style.select_one('div > p')
+    title = title_tag['title']
+    
+    print(info_tag.text)
+    #content > div > div.blog.paginated.home-loop > div
 
-    title = title_tag.text()
-
-print(title)
+# print(beer_style)
